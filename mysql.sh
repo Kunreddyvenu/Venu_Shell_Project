ce12d0173899939657dc4ad951dcf7d4ea7f8f50
venu <kunreddyvenu88@gmail.com>
2026-03-27 13:33:14 +0530

    4th changes

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(basename "$0" .sh))
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
