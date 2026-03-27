910cc18a8f18367bb20afe116a130899547c1a85
venu <kunreddyvenu88@gmail.com>
2026-03-27 15:26:18 +0530

    4th changes

--- backend.sh
+++ backend.sh
@@ -39,7 +39,7 @@ dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "installing nodejs"
 
 id expense &>>$LOGFILE
-if [ $? -ne o ]
+if [ $? -ne 0 ]
 then
    useradd expense &>>$LOGFILE
    VALIDATE $? "creating expense user"
