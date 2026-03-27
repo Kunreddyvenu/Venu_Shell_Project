106fd8d12e76b5e895a376fef105cf3de0a4889f
venu <kunreddyvenu88@gmail.com>
2026-03-27 14:57:22 +0530

    backend 1st version

---
+++ backend.sh
@@ -0,0 +1,88 @@
+#!bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "please enter DB password"
+read -s mysql_root_password
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2 ..$R Failure $N"
+    exit 1 #manually exit code if error comes
+    else
+       echo -e "$2 ..$G Success $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+   echo "pease run the script wit root access."
+   exit 1 #manually exit from command.
+else
+   echo "You are a super user."
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "enabling nodejs 20 version"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "installing nodejs"
+
+id expense &>>$LOGFILE
+if [ $? -ne o ]
+then
+   useradd expense &>>$LOGFILE
+   VALIDATE $? "creating expense user"
+else
+   echo -e "Expense user already created $Y Skipping $N"
+fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "downloading code file"
+
+cd /app
+rm-rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "extracted bacend code"
+
+npm install &>>$LOGFILE
+VALIDATE $? "installing node js dependencies"
+
+#check your repo and path
+#we are giving the absalute patch of backend service to avoid erros
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "installing MYSQL client"
+
+mysql -h 3.86.153.191 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "backend service restarting"
+
+
+
